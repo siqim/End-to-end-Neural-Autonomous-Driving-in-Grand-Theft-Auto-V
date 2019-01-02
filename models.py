8534cd7bb67410e5fe470b9bbed458d3954160cd
Siqi Miao <msq96@qq.com>
2019-01-01 23:59:53 -0600

    update

--- models.py
+++ models.py
@@ -13,12 +13,15 @@ import pretrainedmodels
 
 class Encoder(nn.Module):
 
-    def __init__(self, seq_len, decoder_batch_size, model_name='xception', show_feature_dims=False):
+    def __init__(self, seq_len, decoder_batch_size, freeze=True, model_name='xception', show_feature_dims=False):
         super().__init__()
         self.seq_len = seq_len
         self.decoder_batch_size = decoder_batch_size
 
         self.model = pretrainedmodels.__dict__[model_name](num_classes=1000, pretrained='imagenet')
+        if freeze:
+            for param in self.model.parameters():
+                param.requires_grad_(requires_grad=False)
 
         self.mean = self.model.mean
         self.std = self.model.std
@@ -71,19 +74,19 @@ class Encoder(nn.Module):
 
 class Attention(nn.Module):
 
-    def __init__(self, encoder_dim, decoder_dim, attention_dim, dropout_prob):
+    def __init__(self, num_loc, encoder_dim, decoder_dim, attention_dim, dropout_prob):
         super().__init__()
 
         self.encoder_attention = nn.Linear(encoder_dim, attention_dim)
         self.decoder_attention = nn.Linear(decoder_dim, attention_dim)
         self.full_attention = nn.Sequential(
                 nn.ReLU(),
-                nn.BatchNorm1d(attention_dim),
+                nn.BatchNorm1d(num_loc),
                 nn.Dropout(p=dropout_prob),
 
                 nn.Linear(attention_dim, attention_dim),
                 nn.ReLU(),
-                nn.BatchNorm1d(attention_dim),
+                nn.BatchNorm1d(num_loc),
                 nn.Dropout(p=dropout_prob),
 
                 nn.Linear(attention_dim, 1)
@@ -99,9 +102,9 @@ class Attention(nn.Module):
             encoder_outputs: Output features from encoder with size: decoder_batch_size x seq_len x num_loc x encoder_dim
 
         Returns:
-            encoder_atts: attention of encoder with size: encoder_batch_size x num_loc x attention_dim
+            encoder_atts: attention of encoder with size: decoder_batch_size x seq_len x num_loc x attention_dim
         """
-        encoder_atts = self.encoder_attention(encoder_outputs)  # encoder_batch_size x num_loc x attention_dim
+        encoder_atts = self.encoder_attention(encoder_outputs)  # decoder_batch_size x seq_len x num_loc x attention_dim
         return encoder_atts
 
 
@@ -110,35 +113,42 @@ class Attention(nn.Module):
         Attention forward.
 
         Params:
-            encoder_output: Output of encoder for one image with size: num_loc x encoder_dim
-            encoder_att: Attention of encoder for one image with size: num_loc x attention_dim
-            decoder_state: Hidden state of decoder with size: decoder_batch_size x decoder_dim
-                                                             (decoder_batch_size = 1 for this implementation)
+            encoder_output: Output of encoder for a batch image at same time step with size:
+                            decoder_batch_size x num_loc x encoder_dim
+            encoder_att: Attention of encoder for a batch image at same time step with size:
+                            decoder_batch_size x num_loc x attention_dim
+            decoder_state: Hidden state of decoder with size: num_layers x decoder_batch_size x decoder_dim
 
         Returns:
-            alpha: Weight for each location of encoder_output with size: 1 x num_loc
-            attention_output: attention weighted encoding with size: 1 x encoder_dim
+            alpha: Weight for each location of encoder_output with size: decoder_batch_size x num_loc x 1
+            attention_output: attention weighted encoding with size: decoder_batch_size x encoder_dim
         """
 
-        decoder_att = self.decoder_attention(decoder_state)  # decoder_batch_size x attention_dim
-        full_att = self.full_attention(encoder_att + decoder_att).t()  # 1 x num_loc
+        decoder_att = self.decoder_attention(decoder_state).unsqueeze(2)  # num_layers x decoder_batch_size x 1 x attention_dim
+        full_att = self.full_attention(encoder_att + decoder_att.sum(dim=0))  # decoder_batch_size x num_loc x 1
 
-        alpha = self.softmax(full_att)  # 1 x num_loc
-        attention_output = alpha @ encoder_output  # 1 x encoder_dim
+        alpha = self.softmax(full_att)  # decoder_batch_size x num_loc x 1
+        attention_output = (alpha * encoder_output).sum(dim=1)  # decoder_batch_size x encoder_dim
         return alpha, attention_output
 
 
 class Decoder(nn.Module):
 
-    def __init__(self, encoder_dim, decoder_dim, attention_dim, num_actions, dropout_prob=0.5, num_layers=1, decoder_batch_size=1):
+    def __init__(self, num_loc, encoder_dim, decoder_dim, attention_dim, num_actions,
+                 decoder_batch_size, seq_len, num_layers, dropout_prob=0.5):
         super().__init__()
 
-        self.att_model = Attention(encoder_dim, decoder_dim, attention_dim, dropout_prob)
-
+        self.num_loc = num_loc
+        self.encoder_dim = encoder_dim
         self.decoder_dim = decoder_dim
-        self.decoder_batch_size = decoder_batch_size
+        self.attention_dim = attention_dim
         self.num_actions = num_actions
+        self.decoder_batch_size = decoder_batch_size
+        self.seq_len = seq_len
         self.num_layers = num_layers
+        self.dropout_prob = dropout_prob
+
+        self.att_model = Attention(num_loc, encoder_dim, decoder_dim, attention_dim, dropout_prob)
 
         self.lstm = nn.LSTM(
                 input_size = encoder_dim + num_actions,
@@ -168,31 +178,32 @@ class Decoder(nn.Module):
         Decoder forward.
 
         Params:
-            encoder_outputs: Output features from encoder with size: encoder_batch_size x num_loc x encoder_dim
-            actions: True actions at each time step with size: encoder_batch_size x num_actions
+            encoder_outputs: Output features from encoder with size: decoder_batch_size x seq_len x num_loc x encoder_dim
+            actions: True actions at each time step with size: decoder_batch_size x seq_len x num_actions
 
         Returns:
-            y: Predicted actions from decoder with size: encoder_batch_size x num_actions
+            y: Predicted actions from decoder with size: decoder_batch_size x seq_len x num_actions
         """
 
-        encoder_atts = self.att_model.generate_encoder_atts(encoder_outputs)  # encoder_batch_size x num_loc x attention_dim
-        encoder_batch_size = encoder_atts.size(0)
-        y = torch.zeros((encoder_batch_size, self.num_actions)).cuda()
+        encoder_atts = self.att_model.generate_encoder_atts(encoder_outputs)  # decoder_batch_size x seq_len x num_loc x attention_dim
+        y = torch.zeros((self.decoder_batch_size, self.seq_len, self.num_actions)).cuda()
 
         decoder_state = self._init_state()
 
-        for i in range(encoder_batch_size):
-            h = decoder_state[0]  # num_layers x decoder_batch_size x decoder_dim
+        for i in range(self.seq_len):
 
-            # (1 x num_loc, 1 x encoder_dim)
-            alpha, attention_output = self.att_model.forward(encoder_outputs[i], encoder_atts[i], h[0])
+            # (decoder_batch_size x num_loc x 1, decoder_batch_size x encoder_dim)
+            alpha, attention_output = self.att_model.forward(encoder_outputs[:, i], encoder_atts[:, i], decoder_state[0])
 
-            decoder_input = torch.cat((attention_output, actions[[i]]), dim=1).unsqueeze(0)  # 1 x 1 x (encoder_dim + num_actions)
+            # decoder_batch_size x 1 x (encoder_dim + num_actions)
+            # 1 means sequence length for decoder; since we have a for loop, seq_len here = 1
+            decoder_input = torch.cat((attention_output, actions[:, i]), dim=1).unsqueeze(1)
 
-            # (1 x 1 x decoder_dim, 1 x 1 x decoder_dim), (num_layers x decoder_batch_size x decoder_dim)*2
+            # (decoder_batch_size x 1 x decoder_dim, (num_layers x decoder_batch_size x decoder_dim)*2)
+            # 1 means sequence length for decoder; since we have a for loop, seq_len here = 1
             output, decoder_state = self.lstm(decoder_input, decoder_state)
 
-            y[i] = self.sigmoid(self.fc_output(output[0][0])).squeeze(0)  # num_actions
+            y[:, i] = self.sigmoid(self.fc_output(output.squeeze(1)))  # decoder_batch_size x num_actions
 
         return y
 
@@ -202,20 +213,24 @@ if __name__ == '__main__':
     import time
     import numpy as np
 
-    encoder_batch_size, num_actions = 8, 3
-    decoder_dim, attention_dim = 1024, 1024
+    seq_len, decoder_batch_size, num_actions, num_layers = 56, 2, 3, 2
+    decoder_dim, attention_dim = 512, 512
+    lr = 4e-4
 
-    encoder = Encoder(model_name='xception', show_feature_dims=False)
+    encoder = Encoder(seq_len=seq_len, decoder_batch_size=decoder_batch_size,
+                      freeze=True, model_name='xception', show_feature_dims=True)
     encoder.cuda()
     encoder.train()
 
     mean, std, input_size = encoder.mean, encoder.std, encoder.input_size
     num_loc, encoder_dim = encoder.num_loc, encoder.encoder_dim
 
-    input_images = torch.rand((encoder_batch_size, input_size[0], input_size[1], input_size[2])).cuda()
-    actions = torch.rand((encoder_batch_size, num_actions)).cuda()
+    input_images = torch.rand((decoder_batch_size, seq_len, input_size[0], input_size[1], input_size[2])).cuda()
+    actions = torch.rand((decoder_batch_size, seq_len, num_actions)).cuda()
 
-    decoder = Decoder(encoder_dim=2048, decoder_dim=decoder_dim, attention_dim=attention_dim, num_actions=num_actions, flag='lstm', num_layers=1)
+    decoder = Decoder(num_loc=num_loc, encoder_dim=encoder_dim, decoder_dim=decoder_dim, attention_dim=attention_dim,
+                      num_actions=num_actions, decoder_batch_size=decoder_batch_size, seq_len=seq_len,
+                      num_layers=num_layers)
     decoder.cuda()
     decoder.train()
 
@@ -223,7 +238,7 @@ if __name__ == '__main__':
     trainloss = 0
     criterion = nn.MSELoss()
     model_paras = list(encoder.parameters()) + list(decoder.parameters())
-    optimizer = optim.Adam(model_paras, lr=4e-4)
+    optimizer = optim.Adam(model_paras, lr=lr)
     for i in range(100):
         s = time.time()
 
@@ -234,6 +249,7 @@ if __name__ == '__main__':
 
         loss = criterion(y, actions)
         loss.backward()
+
         optimizer.step()
 
         e = time.time()
